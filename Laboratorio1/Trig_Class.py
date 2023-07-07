b53e05ab3f1f99c5067cbb33f3a2404ba919cc35
miguelalvarad0 <130612166+miguelalvarad0@users.noreply.github.com>
2023-07-06 21:55:01 -0600

    Create Trig_Class.py

---
+++ Laboratorio1/Trig_Class.py
@@ -0,0 +1,21 @@
+import math
+
+
+class Trig():
+    
+    def __init__(self):
+        self.pi = math.pi
+    
+    def sin():
+        sin = math.sin(math.pi)
+        return(sin)   
+    
+    def cos():
+        cos = math.cos(math.pi)
+        return(cos)
+        #print(f"El seno de pi es: {cos}")
+
+    def tan():
+        tan = math.tan(math.pi)
+        return(tan)
+        #print(f"El seno de pi es: {tan}")
