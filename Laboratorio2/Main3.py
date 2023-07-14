261ea8f10e351d196e8997ac0fb34924e55f8b7a
miguelalvarad0 <miguelalvaradohi23@gmail.com>
2023-07-13 23:59:11 -0600

    'THREAD' METHOD INCLUDED

--- Laboratorio2/Main3.py
+++ Laboratorio2/Main3.py
@@ -2,6 +2,8 @@ import requests
 import time
 import api
 import ids
+import concurrent.futures
+import threading
 
 # ----------------------------------------------------------------------------
 # DOWNLOAD USERNAMES USING NO CONCURRENCY METHOD 
@@ -22,18 +24,56 @@ import ids
 
 # ----------------------------------------------------------------------------
 # DOWNLOAD USERNAMES USING ASYNCIO 
-def download_all_sites(sites):
-     with requests.Session() as session:
+# def download_all_sites(sites):
+#      with requests.Session() as session:
+#         for i in range(0,150):
+#             user = api.getOneUser_asyncio2(ids.ids[i],session)
+#             print(user)
+
+
+# if __name__ == "__main__":
+    
+#     sites = ["https://jsonplaceholder.typicode.com/users/"]
+#     start_time = time.time()
+#     download_all_sites(sites)
+#     duration = time.time() - start_time
+#     print(f"Downloaded in {duration} seconds, using aosync concurrency method")
+# ----------------------------------------------------------------------------
+
+
+
+# ----------------------------------------------------------------------------
+# DOWNLOAD USERNAMES USING THREAD
+
+thread_local = threading.local()
+
+
+def get_session():
+    if not hasattr(thread_local, "session"):
+        thread_local.session = requests.Session()
+    return thread_local.session
+
+
+def download_site(sites):
+    session = get_session()
+    with session.get(sites) as response:
         for i in range(0,150):
-            user = api.getOneUser_asyncio2(ids.ids[i],session)
+            user = api.getOneUser_thread(ids.ids[i],response)
             print(user)
 
 
+def download_all_sites(sites):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        executor.map(download_site, sites)
+        
+
+
 if __name__ == "__main__":
-    
-    sites = ["https://jsonplaceholder.typicode.com/users/"]
+    sites = [
+        "https://jsonplaceholder.typicode.com/users/",
+    ]
     start_time = time.time()
     download_all_sites(sites)
     duration = time.time() - start_time
-    print(f"Downloaded in {duration} seconds, using aosync concurrency method")
-# ----------------------------------------------------------------------------
+    print(f"Downloaded in {duration} seconds, using thread concurency method")
+# -------------------------------------------------------------------------------------
--- Laboratorio2/api.py
+++ Laboratorio2/api.py
@@ -7,11 +7,11 @@ def getOneUser(id):
     return response.json()[id]['name']
 
 
-
 def getOneUser_asyncio2(id,session):
     url = "https://jsonplaceholder.typicode.com/users/"
     response = session.get(url)
     return response.json()[id]['name']
-          
 
-          
+
+def getOneUser_thread(id,response):
+    return response.json()[id]['name']
